8dc84ef0244bd35cdc2b9bafe4e568ab21217727
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-21 11:29:26 -0400

    Create FileParser.java

---
+++ src/main/java/FileParser.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileParser {
+	
+	public static List<String> readTextFileByLines(String fileName) throws IOException{
+		List<String> lines = Files.readAllLines(Paths.get(fileName));
+		return lines;
+	}
+	
+	public static void getCurrentDirectory(){
+		System.out.println(System.getProperty("user.dir"));
+	}
+	
+	public static List<String> readYahooStcokFileByLines(String fileName) throws IOException{
+		List<String> lines = Files.readAllLines(Paths.get(fileName));
+		lines.remove(0);
+		return lines;
+	}
+	
+	
+	
+	
+}
