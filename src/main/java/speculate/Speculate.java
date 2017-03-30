a6025f615bac1767fb906507ce6fafb5127368af
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-30 14:01:35 -0400

    added equity, risk and moving avg

--- src/main/java/speculate/Speculate.java
+++ src/main/java/speculate/Speculate.java
@@ -5,8 +5,15 @@ public interface Speculate {
 	String BACK_TEST = "Back Test";
 	String LIVE = "Live";
 	
+	BigDecimal STOCK_EQUITY = new BigDecimal(100_000.00); 
+	BigDecimal DIGITAL_EQUITY = new BigDecimal(10.00000000);
+	
+	BigDecimal RISK = new BigDecimal(0.01);
+	
 	int ENTRY = 25;
 	int EXIT  = 10;
+	
+	int MOVING_AVG = 20;
 	
 	void run();
 	
