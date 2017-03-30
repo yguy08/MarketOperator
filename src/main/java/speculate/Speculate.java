748dbaba014cc37289eb90189e331b132c8b0546
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-30 15:29:16 -0400

    add stop

--- src/main/java/speculate/Speculate.java
+++ src/main/java/speculate/Speculate.java
@@ -1,5 +1,10 @@
 package speculate;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import entry.Entry;
+
 public interface Speculate {
 
 	String BACK_TEST = "Back Test";
@@ -9,6 +14,8 @@ public interface Speculate {
 	BigDecimal DIGITAL_EQUITY = new BigDecimal(10.00000000);
 	
 	BigDecimal RISK = new BigDecimal(0.01);
+	
+	BigDecimal STOP = new BigDecimal(2.00);
 	
 	int ENTRY = 25;
 	int EXIT  = 10;
