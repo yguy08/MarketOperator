fb943eb7aa3ac8075c3d8153d3540180416d44c3
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-30 15:26:10 -0400

    latest exit

---
+++ src/main/java/exit/DigitalExit.java
@@ -0,0 +1,14 @@
+package exit;
+
+import asset.Asset;
+import entry.Entry;
+import market.Market;
+import position.Position;
+
+public class DigitalExit implements Exit {
+	
+	public DigitalExit(Market market, Asset asset, Entry entry, Position position){
+		
+	}
+
+}
