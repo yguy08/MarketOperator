2512bc6eaf603c82a4e601b8b9042f8bf0730ded
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-30 10:14:34 -0400

    create digital entry

---
+++ src/main/java/entry/DigitalEntry.java
@@ -0,0 +1,110 @@
+package entry;
+
+import java.math.BigDecimal;
+
+import asset.Asset;
+import market.Market;
+
+public class DigitalEntry implements Entry {
+	
+	public DigitalEntry(Market market, Asset asset){
+		
+	}
+
+	@Override
+	public void setEntry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Entry getEntry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCurrentPrice() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BigDecimal getCurrentPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMaxPrice() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BigDecimal getMaxPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMinPrice() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BigDecimal getMinPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDirection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLocationAsIndex() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLocationIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Boolean isEntry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPriceSubList(Asset asset) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
