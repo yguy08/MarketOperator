cd04a679ddbd5e61a3a359d5c39f2bc3b541e10e
Wyatt Endres <wyatt.endres@gmail.com>
2017-03-30 13:47:26 -0400

    added equity and risk final variables

--- src/main/java/market/Market.java
+++ src/main/java/market/Market.java
@@ -6,6 +6,10 @@ public interface Market {
 	
 	public static final String DIGITAL_MARKET = "Digital Market";
 	public static final String STOCK_MARKET   = "Stock Market";
+	
+	public static final BigDecimal STOCK_EQUITY = new BigDecimal(100_000.00); 
+	public static final BigDecimal DIGITAL_EQUITY = new BigDecimal(10.00000000);
+	public static final BigDecimal RISK = new BigDecimal(0.01);
 		
 	String getMarketName();
 	
