afc54d0a0a7b5bc3dee01db2a42b3a1e502df84e
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-05 08:13:06 -0400

    genrate yahoo url and read etf list

--- src/main/java/fileparser/FileParser.java
+++ src/main/java/fileparser/FileParser.java
@@ -2,6 +2,7 @@ package fileparser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileParser {
@@ -16,21 +17,43 @@ public class FileParser {
 	}
 	
 	public static List<String> readYahooStockFileByLines(String fileName) throws IOException{
-		String csv = fileName + ".csv";
+		String csv = "StockFiles/" + fileName + ".csv";
 		List<String> lines = Files.readAllLines(Paths.get(csv));
 		lines.remove(0);
 		return lines;
 	}
 	
 	public static List<String> readFedForexByLines(String fileName) throws IOException{
-	List<String> lines = Files.readAllLines(Paths.get(fileName));
+	List<String> lines = Files.readAllLines(Paths.get("StockFiles/" + fileName));
 	lines.remove(0);
 		return lines;
 	}
 	
 	public static List<String> readStockTickerList() throws IOException{
-		List<String> lines = Files.readAllLines(Paths.get("StockTickerList.txt"));
+		List<String> lines = Files.readAllLines(Paths.get("StockFiles/SP500.csv"));
 		return lines;
+	}
+	
+	public static List<String> readETFList() throws IOException{
+		List<String> lines = Files.readAllLines(Paths.get("StockFiles/TopEtfs.csv"));
+		return lines;
+	}
+	
+	public static List<String> generateYahooURL() {
+		try {
+			List<String> etfs = readETFList();
+			List<String> urls = new ArrayList<>();
+			for(int i = 0; i < etfs.size();i++){
+				String url = "http://chart.finance.yahoo.com/table.csv?s=" +etfs.get(i)+"&a=3&b=5&c=2012&d=3&e=5&f=2017&g=d&ignore=.csv";
+				urls.add(url);
+			}
+			return urls;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
 	}
 	
 	
