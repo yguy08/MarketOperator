108ad5039b5708c15468b772450c8f0527f9c597
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-05 16:50:24 -0400

    entry interface

--- src/main/java/entry/Entry.java
+++ src/main/java/entry/Entry.java
@@ -2,12 +2,7 @@ package entry;
 
 import java.math.BigDecimal;
 
-import asset.Asset;
-
 public interface Entry {
-	
-	String LONG = "Long";
-	String SHORT = "Short";
 	
 	void setEntry();
 	
@@ -52,7 +47,5 @@ public interface Entry {
 	void setOrderTotal();
 	
 	BigDecimal getOrderTotal();
-	
-	void updateAccountBalance();
 
 }
