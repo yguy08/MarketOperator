0059bd4aa8c21a262f194ccc0af85100a335cace
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-05 16:55:07 -0400

    update speculate

--- src/main/java/speculate/Speculate.java
+++ src/main/java/speculate/Speculate.java
@@ -3,9 +3,6 @@ package speculate;
 import java.math.BigDecimal;
 
 import asset.Asset;
-import asset.AssetFactory;
-import backtest.BackTest;
-import backtest.BackTestFactory;
 import market.Market;
 
 public interface Speculate {
@@ -13,24 +10,39 @@ public interface Speculate {
 	String BACK_TEST = "Back Test";
 	String LIVE = "Live";
 	
+	String LONG = "Long";
+	String SHORT = "Short";
+	
 	BigDecimal STOCK_EQUITY = new BigDecimal(10_000.00); 
 	BigDecimal DIGITAL_EQUITY = new BigDecimal(3.00000000);
 	
 	BigDecimal RISK = new BigDecimal(0.01);
-	
 	BigDecimal STOP = new BigDecimal(2.00);
 	
 	int ENTRY = 25;
 	int EXIT  = 10;
-	
 	int MOVING_AVG = 20;
 	
 	void setAccountEquity(BigDecimal tradeResult);
 	
 	BigDecimal getAccountEquity();
 	
-	void getAllEntries();
+	void setTotalReturnPercent();
+	
+	BigDecimal getTotalReturnPercent();
+	
+	void getAllEntriesSingleMarket(Market market);
 
-	void runBackTestOnAllMarkets(Market market);
+	void backTestAllAssetsSingleMarket(Market market);
+	
+	void backTestSingleAsset(Market market, Asset asset);
+	
+	default void getAllEntriesAllMarkets() {
+		
+	}
+	
+	default void backTestAllAssetsAllMarkets(){
+		
+	}
 	
 }
