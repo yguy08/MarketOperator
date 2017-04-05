27e1dbaa9971e11ab61f5cd964e71f989df6d21b
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-05 16:49:20 -0400

    stock back test

--- src/main/java/backtest/StockBackTest.java
+++ src/main/java/backtest/StockBackTest.java
@@ -32,22 +32,23 @@ public class StockBackTest implements BackTest {
 	public void runBackTest() {
 		for(int x = Speculate.ENTRY; x < this.asset.getPriceList().size();x++){
 			this.asset.setPriceSubList(x - Speculate.ENTRY, x + 1);
-			entry = entryFactory.findEntry(this.market, this.asset, this.speculator);
+			entry = entryFactory.findEntry(this.market, this.asset);
 			if(entry.isEntry()){
 				//System.out.println(entry.toString());
 				for(int y = this.entry.getLocationIndex();y<this.asset.getPriceList().size() || position.isOpen() == false; y++, x++){
 					this.asset.setPriceSubList(y - Speculate.EXIT, y + 1);
-					this.position = positionFactory.createPosition(this.market, this.asset, this.entry, this.speculator);
+					this.position = positionFactory.createPosition(this.market, this.asset, this.entry);
 					if(position.isOpen() == false){
-						System.out.println(position.toString());
+						//System.out.println(entry.toString());
+						//System.out.println(position.toString());
+						this.speculator.setAccountEquity(this.position.getProfitLossAmount());
+						this.speculator.setTotalReturnPercent();
+						//System.out.println(this.speculator);
 						break;
 					}
 				}
 			}
-		}
-		
-		System.out.println(this.speculator);
-		
+		}		
 	}
 
 }
