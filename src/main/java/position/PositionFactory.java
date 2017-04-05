b4ff230331b7e46a8c28c61b4d07e2ff19c6184a
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-05 16:53:46 -0400

    update position factory

--- src/main/java/position/PositionFactory.java
+++ src/main/java/position/PositionFactory.java
@@ -7,15 +7,15 @@ import speculate.Speculate;
 
 public class PositionFactory {
 	
-	public Position createPosition(Market market, Asset asset, Entry entry, Speculate speculator){
+	public Position createPosition(Market market, Asset asset, Entry entry){
 		if(market == null){
 			return null;
 		}
 		
 		if(market.getMarketName().equalsIgnoreCase(Market.STOCK_MARKET)){
-			return new StockPosition(market, asset, entry, speculator);
+			return new StockPosition(market, asset, entry);
 		}else if(market.getMarketName().equalsIgnoreCase(Market.DIGITAL_MARKET)){
-			return new DigitalPosition(market, asset, entry, speculator);
+			return new DigitalPosition(market, asset, entry);
 		}
 		
 		return null;
