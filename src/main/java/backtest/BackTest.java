e6e388e195ee7cd46888f132b09958d30948232a
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-06 16:40:22 -0400

    latest backtest

--- src/main/java/backtest/BackTest.java
+++ src/main/java/backtest/BackTest.java
@@ -1,7 +1,24 @@
 package backtest;
 
+import java.util.List;
+
+import entry.Entry;
+import position.Position;
+
 public interface BackTest {
 	
 	void runBackTest();
+	
+	void setEntryList(Entry entry);
+	
+	List<Entry> getEntryList();
+	
+	void setPositionList(Position position);
+	
+	List<Position> getPositionList();
+	
+	Entry getLastEntry();
+	
+	Position getLastPosition();
 	
 }
