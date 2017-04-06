13f968f8647254d6da576e27b24d05eebe030b0b
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-06 16:27:01 -0400

    get latest entries, positions

--- src/main/java/speculate/StockSpeculation.java
+++ src/main/java/speculate/StockSpeculation.java
@@ -3,6 +3,7 @@ package speculate;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.List;
 
 import asset.Asset;
 import asset.AssetFactory;
@@ -46,30 +47,31 @@ public class StockSpeculation implements Speculate {
 	}
 
 	@Override
-	public void getAllEntriesSingleMarket(Market market) {
-		AssetFactory assetFactory = new AssetFactory();
-		EntryFactory entryFactory = new EntryFactory();
-		for(int i = 0; i < market.getAssets().size();i++){
+	public void getLatestEntriesSingleMarket(Market market) {
+		//REMOVE SPECULATE WHEN REMOVED FROM BACKTEST
+		SpeculateFactory speculateFactory = new SpeculateFactory();
+		Speculate speculate = speculateFactory.startSpeculating(market);
+		for(int i=0; i < market.getAssets().size();i++){
+			AssetFactory assetFactory = new AssetFactory();
 			Asset asset = assetFactory.createAsset(market, market.getAssets().get(i).toString());
-			asset.setPriceSubList(asset.getPriceList().size() - Speculate.ENTRY, asset.getPriceList().size());
-			Entry entry = entryFactory.findEntry(market, asset);
-			if(entry.isEntry()){
-				System.out.println(entry.toString());
-			}
+			BackTestFactory backTestFactory = new BackTestFactory();
+			BackTest backtest = backTestFactory.newBackTest(market, asset, speculate);
+			System.out.println(backtest.getLastEntry().toString());
 		}		
 	}
 
 	@Override
-	public void backTestAllAssetsSingleMarket(Market market) {
-		AssetFactory assetFactory = new AssetFactory();
+	public void getAllOpenPositionsSingleMarket(Market market) {
+		//REMOVE SPECULATE WHEN REMOVED FROM BACKTEST
 		SpeculateFactory speculateFactory = new SpeculateFactory();
 		Speculate speculate = speculateFactory.startSpeculating(market);
 		for(int i=0; i < market.getAssets().size();i++){
-				Asset asset = assetFactory.createAsset(market, market.getAssets().get(i).toString());
-				BackTestFactory backTestFactory = new BackTestFactory();
-				BackTest backtest = backTestFactory.newBackTest(market, asset, speculate);
-				System.out.println(asset.getAsset() + " " + speculate.toString());
-			}
+			AssetFactory assetFactory = new AssetFactory();
+			Asset asset = assetFactory.createAsset(market, market.getAssets().get(i).toString());
+			BackTestFactory backTestFactory = new BackTestFactory();
+			BackTest backtest = backTestFactory.newBackTest(market, asset, speculate);
+			System.out.println(backtest.getLastPosition().toString());
+		}
 	}
 
 	@Override
@@ -78,7 +80,35 @@ public class StockSpeculation implements Speculate {
 		Speculate speculate = speculateFactory.startSpeculating(market);
 		BackTestFactory backTestFactory = new BackTestFactory();
 		BackTest backtest = backTestFactory.newBackTest(market, asset, speculate);
-		System.out.println(asset.getAsset() + " " + speculate.toString());
+		for(int i = 0; i < backtest.getEntryList().size();i++){
+			speculate.setAccountEquity(backtest.getPositionList().get(i).getProfitLossAmount());
+			speculate.setTotalReturnPercent();
+			System.out.println(backtest.getEntryList().get(i).toString());
+			System.out.println(backtest.getPositionList().get(i).toString());
+			System.out.println(speculate.toString());
+		}
+		
+		
+	}
+	
+	@Override
+	public void backTestAllAssetsSingleMarket(Market market) {
+		SpeculateFactory speculateFactory = new SpeculateFactory();
+		Speculate speculate = speculateFactory.startSpeculating(market);
+		for(int i=0; i < market.getAssets().size();i++){
+				AssetFactory assetFactory = new AssetFactory();
+				Asset asset = assetFactory.createAsset(market, market.getAssets().get(i).toString());
+				BackTestFactory backTestFactory = new BackTestFactory();
+				BackTest backtest = backTestFactory.newBackTest(market, asset, speculate);
+				for(int x = 0; x < backtest.getEntryList().size();x++){
+					speculate.setAccountEquity(backtest.getPositionList().get(x).getProfitLossAmount());
+					speculate.setTotalReturnPercent();
+					System.out.println(backtest.getEntryList().get(x).toString());
+					System.out.println(backtest.getPositionList().get(x).toString());
+					System.out.println(speculate.toString());
+				}
+			}
+	
 	}
 	
 	@Override
