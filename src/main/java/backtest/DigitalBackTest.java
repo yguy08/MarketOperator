40c7ec3d5b672dc254a42fd705d8dff6fe432139
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-06 16:39:29 -0400

    latest digital backtest

--- src/main/java/backtest/DigitalBackTest.java
+++ src/main/java/backtest/DigitalBackTest.java
@@ -1,5 +1,8 @@
 package backtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import asset.Asset;
 import entry.Entry;
 import entry.EntryFactory;
@@ -12,18 +15,18 @@ public class DigitalBackTest implements BackTest {
 	
 	Market market;
 	Asset asset;
-	Speculate speculator;
 	
 	EntryFactory entryFactory = new EntryFactory();
 	Entry entry;
+	List<Entry> entryList = new ArrayList<>();
 	
 	PositionFactory positionFactory = new PositionFactory();
 	Position position;
+	List<Position> positionList = new ArrayList<>();
 	
 	public DigitalBackTest(Market market, Asset asset, Speculate speculate){
 		this.market = market;
 		this.asset = asset;
-		this.speculator = speculate;
 		runBackTest();
 	}
 
@@ -33,21 +36,52 @@ public class DigitalBackTest implements BackTest {
 			this.asset.setPriceSubList(x - Speculate.ENTRY, x + 1);
 			entry = entryFactory.findEntry(this.market, this.asset);
 			if(entry.isEntry()){
-				System.out.println(entry.toString());
+				setEntryList(entry);
 				for(int y = this.entry.getLocationIndex();y<this.asset.getPriceList().size() || position.isOpen() == false; y++, x++){
 					this.asset.setPriceSubList(y - Speculate.EXIT, y + 1);
 					this.position = positionFactory.createPosition(this.market, this.asset, this.entry);
 					if(position.isOpen() == false){
-						System.out.println(entry.toString());
-						System.out.println(position.toString());
-						this.speculator.setAccountEquity(this.position.getProfitLossAmount());
-						this.speculator.setTotalReturnPercent();
-						System.out.println(this.speculator);
+						setPositionList(position);
+						break;
+					}else if(position.isOpen() && x == this.asset.getPriceList().size() - 1){
+						setPositionList(position);
 						break;
 					}
 				}
 			}
 		}
+	}
+
+	@Override
+	public void setEntryList(Entry entry) {
+		this.entryList.add(entry);
+		
+	}
+
+	@Override
+	public List<Entry> getEntryList() {
+		return this.entryList;
+	}
+
+	@Override
+	public void setPositionList(Position position) {
+		this.positionList.add(position);
+		
+	}
+
+	@Override
+	public List<Position> getPositionList() {
+		return this.positionList;
+	}
+
+	@Override
+	public Entry getLastEntry() {
+		return this.entryList.get(this.entryList.size()-1);
+	}
+
+	@Override
+	public Position getLastPosition() {
+		return this.positionList.get(this.positionList.size()-1);
 	}
 
 }
