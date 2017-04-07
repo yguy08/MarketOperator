a7f16e5a0a0452340ed3005a2c4744195957fe95
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-07 15:21:57 -0400

    Update BackTestFactory.java

--- src/main/java/backtest/BackTestFactory.java
+++ src/main/java/backtest/BackTestFactory.java
@@ -6,16 +6,16 @@ import speculate.Speculate;
 
 public class BackTestFactory {
 	
-	public BackTest newBackTest(Market market, Asset asset, Speculate speculate){
+	public BackTest newBackTest(Market market, Asset asset, Speculate speculator){
 		
 		if(market == null){
 		return null;
 		}
 		
 		if(market.getMarketName() == Market.STOCK_MARKET){
-			return new StockBackTest(market, asset, speculate);
+			return new StockBackTest(market, asset, speculator);
 		}else if(market.getMarketName() == Market.DIGITAL_MARKET){
-			return new DigitalBackTest(market, asset, speculate);
+			return new DigitalBackTest(market, asset, speculator);
 		}
 		
 		return null;
