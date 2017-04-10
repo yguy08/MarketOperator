15564684bfba490f75a9e898f622b081d0b2757b
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-10 15:41:57 -0400

    update file parser demo

--- src/main/java/fileparser/FileParserDemo.java
+++ src/main/java/fileparser/FileParserDemo.java
@@ -5,12 +5,25 @@ import java.util.List;
 public class FileParserDemo {
 
 	public static void main(String[] args) {
-		List<String> etfs = FileParser.generateYahooURL();
+		getEtfURL();
+
+	}
+	
+	static void getEtfURL(){
+		List<String> etfs = FileParser.generateETFYahooURL();
 		
 		for(int i = 0; i < etfs.size();i++){
 			System.out.println(etfs.get(i));
 		}
-
+		
+	}
+	
+	static void getForexUrl(){
+		List<String> forex = FileParser.generateForexYahooURL();
+		
+		for(int i = 0; i < forex.size();i++){
+			System.out.println(forex.get(i));
+		}
 	}
 
 }
