a7bcc90e06f2b46c851a317f89ca2611605c669b
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-11 16:56:09 -0400

    Create FileParserDemo.java

---
+++ src/main/java/utils/FileParserDemo.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.util.List;
+
+public class FileParserDemo {
+
+	public static void main(String[] args) {
+		getEtfURL();
+
+	}
+	
+	static void getEtfURL(){
+		List<String> etfs = FileParser.generateETFYahooURL();
+		
+		for(int i = 0; i < etfs.size();i++){
+			System.out.println(etfs.get(i));
+		}
+		
+	}
+	
+	static void getForexUrl(){
+		List<String> forex = FileParser.generateForexYahooURL();
+		
+		for(int i = 0; i < forex.size();i++){
+			System.out.println(forex.get(i));
+		}
+	}
+
+}
