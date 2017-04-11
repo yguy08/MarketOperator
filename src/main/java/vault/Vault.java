edd5c8839b33a5d7e60702db797fa50bfb412015
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-11 16:57:34 -0400

    Create Vault.java

---
+++ src/main/java/vault/Vault.java
@@ -0,0 +1,24 @@
+package vault;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import market.Market;
+import market.MarketFactory;
+import speculate.Speculate;
+import speculate.SpeculateFactory;
+
+public class Vault {
+	
+	public Market market;
+	public Speculate speculate;
+	public ObservableList<String> resultsList = FXCollections.observableArrayList();;
+	
+	public Vault(String marketName, ObservableList<String> resultsList){
+		MarketFactory marketFactory = new MarketFactory();
+		this.market = marketFactory.createMarket(marketName);
+		SpeculateFactory speculateFactory = new SpeculateFactory();
+		this.speculate = speculateFactory.startSpeculating(this.market);
+		this.resultsList = resultsList;
+	}
+
+}
