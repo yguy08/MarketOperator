d0fbec459f0769d318247e00da228019d8a8620c
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-13 17:00:23 -0400

    Update Vault.java

--- src/main/java/vault/Vault.java
+++ src/main/java/vault/Vault.java
@@ -4,28 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import market.Market;
 import market.MarketFactory;
-import speculate.Speculate;
-import speculate.SpeculateFactory;
 
 public class Vault {
 	
 	public Market market;
-	public Speculate speculate;
 	public ObservableList<String> resultsList = FXCollections.observableArrayList();;
 	
 	public Vault(String marketName, ObservableList<String> resultsList){
 		MarketFactory marketFactory = new MarketFactory();
 		this.market = marketFactory.createMarket(marketName);
-		SpeculateFactory speculateFactory = new SpeculateFactory();
-		this.speculate = speculateFactory.startSpeculating(this.market);
 		this.resultsList = resultsList;
 	}
 	
 	public Vault(String marketName){
 		MarketFactory marketFactory = new MarketFactory();
 		this.market = marketFactory.createMarket(marketName);
-		SpeculateFactory speculateFactory = new SpeculateFactory();
-		this.speculate = speculateFactory.startSpeculating(this.market);
 	}
 
 }
