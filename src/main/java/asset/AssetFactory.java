7f6dc98fc716628aa0e55e744284ab23d4cfa1e4
Wyatt Endres <wyatt.endres@gmail.com>
2017-04-18 16:56:48 -0400

    assets

--- src/main/java/asset/AssetFactory.java
+++ src/main/java/asset/AssetFactory.java
@@ -5,19 +5,16 @@ import market.Market;
 public class AssetFactory {
 	
 	public Asset createAsset(Market market, String assetName){
-		if(market == null){
-			return null;
-		}
-		
-		if(market.getMarketName().equals(Market.STOCK_MARKET)){
-			return new StockAsset(market,assetName);
-		}else if (market.getMarketName().equals(Market.DIGITAL_MARKET)){
+		switch(market.getMarketName()){
+		case Market.DIGITAL_MARKET:
 			return new DigitalAsset(market, assetName);
-		}else if (market.getMarketName().equals(Market.POLONIEX_OFFLINE)){
+		case Market.POLONIEX_OFFLINE:
+			return new PoloniexOfflineAsset(market, assetName);
+		case Market.STOCK_MARKET:
+			return new StockAsset(market, assetName);
+		default:
 			return new PoloniexOfflineAsset(market, assetName);
 		}
-		
-		return null;
 	}
 
 }
