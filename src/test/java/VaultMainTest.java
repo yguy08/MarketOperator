86889cba09bf37059de57e0b7b80e02a15faa7f7
Wyatt Endres <wyatt.endres@gmail.com>
2017-05-05 17:12:30 -0400

    Add files via upload

---
+++ src/test/java/VaultMainTest.java
@@ -0,0 +1,30 @@
+package vault;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class VaultMainTest extends Application {
+	
+	Image icon = new Image(getClass().getResourceAsStream("icon-treesun-64x64.png"));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+        Parent root = FXMLLoader.load(VaultMain.class.getResource("VaultMainFXML.fxml"));
+        Scene scene = new Scene(root, 570, 320);
+        stage.setScene(scene);
+        stage.setTitle("Speculation 1000");
+        stage.getIcons().add(icon);
+        stage.show();
+	}
+
+}
