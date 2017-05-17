e124bbd9e4e2ed0c2d72f2cc930469df1a0b7abd
Wyatt Endres <wyatt.endres@gmail.com>
2017-05-17 16:11:27 -0400

    delete controlled screen stupid framework

--- src/main/java/vault/ControlledScreen.java
+++
@@ -1,7 +0,0 @@
-package vault;
-
-public interface ControlledScreen {
-    
-    //This method will allow the injection of the Parent ScreenPane
-    public void setScreenParent(ScreensController screenPage);
-}
