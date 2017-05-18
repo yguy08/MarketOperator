b52af29028b56fbb4939d6319506a55c8fee4f77
Wyatt Endres <wyatt.endres@gmail.com>
2017-05-18 11:34:12 -0400

    date utils junit

---
+++ src/test/java/util/DateUtilsJunit.java
@@ -0,0 +1,28 @@
+package util;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class DateUtilsJunit {
+	
+	String simpleDateBaseline = "2017-05-18";
+	String dateToSimpleDateFormat = DateUtils.dateToSimpleDateFormat(new GregorianCalendar(2017, Calendar.MAY, 18).getTime());
+	
+	@Test
+	public void testDateToSimpleDateFormat(){
+		assertEquals(simpleDateBaseline, dateToSimpleDateFormat);
+	}
+	
+	String MMddFormatBaseline = "05-18";
+	String dateToMMddFormat = DateUtils.dateToMMddFormat(new GregorianCalendar(2017, Calendar.MAY, 18).getTime());
+	
+	@Test
+	public void testdateToMMddFormat(){
+		assertEquals(MMddFormatBaseline, dateToMMddFormat);
+	}
+	
+
+}
