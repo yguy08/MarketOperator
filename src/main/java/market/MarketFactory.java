18117daa3c4fd659c4bb38c8742432cedae1ea77
Wyatt Endres <wyatt.endres@gmail.com>
2017-05-30 17:13:19 -0400

    Add files via upload

--- src/main/java/market/MarketFactory.java
+++ src/main/java/market/MarketFactory.java
@@ -1,14 +1,26 @@
 package market;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MarketFactory {
 	
-	public static Market createOnlineBitcoinMarket(){
-		return BitcoinMarket.createOnlineBitcoinMarket();
+	//called when app first loads
+	public static Market createMarket(){
+    	//try internet connection to decide online/offline
+		try {
+            URL myURL = new URL("https://poloniex.com/");
+            URLConnection myURLConnection = myURL.openConnection();
+            myURLConnection.connect();
+            return BitcoinMarket.createOnlineBitcoinMarket();
+        } 
+        catch (IOException e) {
+            return BitcoinMarket.createOfflineBitcoinMarket();
+        }
 	}
 	
-	public static Market createOfflineBitcoinMarket(){
-		return BitcoinMarket.createOfflineBitcoinMarket();
-	}
+	//create market (String marketName)
 	
 	//create online dollar market
 	
