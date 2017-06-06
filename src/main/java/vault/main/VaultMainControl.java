0814318885331ab7464ba667e05c9060c84056df
Wyatt <Wyatt.endres@gmail.com>
2017-06-06 08:10:55 -0400

    new results

--- src/main/java/vault/main/VaultMainControl.java
+++ src/main/java/vault/main/VaultMainControl.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 import asset.Asset;
 import entry.Entry;
 import exit.Exit;
@@ -44,6 +49,8 @@ public class VaultMainControl extends BorderPane {
 	@FXML private Button clearList;
 	
 	@FXML private Text statusText;
+	
+	@FXML private Button backTest;
     
 	public VaultMainControl() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VaultMainView.fxml"));
@@ -205,6 +212,87 @@ public class VaultMainControl extends BorderPane {
 		        });
 			}
 		});		
+	}
+	
+	@FXML public void backtest(){
+		mainListViewControl.getMainObservableList().removeAll(mainListViewControl.getMainObservableList());
+		setRandomStatus();
+		Speculator speculator = speculatorControl.getSpeculator();
+		
+		Task<HashMap<List<Entry>,List<Exit>>> task = new Task<HashMap<List<Entry>,List<Exit>>>() {
+		    @Override protected HashMap<List<Entry>,List<Exit>> call() throws Exception {
+		    	List<Entry> entryList = new ArrayList<>();
+				List<Exit> exitList = new ArrayList<>();
+				HashMap<List<Entry>,List<Exit>> hs = new HashMap<>();
+				for(Asset a : market.getAssetList()){
+					List<Entry> e = a.getEntryList(speculator);
+					List<Exit> ex = a.getExitList(speculator);
+					
+					//sort list
+					Collections.sort(e, new Comparator<Entry>() {
+					    @Override
+						public int compare(Entry o1, Entry o2) {
+					        return o1.getDateTime().compareTo(o2.getDateTime());
+					    }
+					});
+					
+					Collections.sort(ex, new Comparator<Exit>() {
+					    @Override
+						public int compare(Exit o1, Exit o2) {
+					        return o1.getExitDate().compareTo(o2.getExitDate());
+					    }
+					});
+					
+					System.out.println(e.size());
+					System.out.println(ex.size());
+					
+					//get start date and number of days since
+					Date startDate = e.get(0).getDateTime();
+					int days = DateUtils.getNumDaysFromDateToToday(startDate);
+										
+					for(int z = 0; z <= days; z++){
+						Date currentDateOfBacktest = DateUtils.addDays(startDate, z);
+						List<Entry> my = new ArrayList<>();
+						List<Exit> exx = new ArrayList<>();
+						for(Entry entry : e){
+							if(entry.getDateTime().equals(currentDateOfBacktest)){
+								my.add(entry);
+							}
+						}
+						
+						for(Exit exit : ex){
+							if(exit.getEntryDate().equals(currentDateOfBacktest)){
+								exx.add(exit);
+							}
+						}
+						hs.put(my, exx);
+					}
+					//add exits to map (SORT DATE DESCINDING?)
+					
+					//for days
+						//for map @ day
+					//if entry and spot open take (or add filter)
+					//else SKIP 
+				}
+		        return hs;
+		    }
+		};
+		
+		new Thread(task).start();
+		task.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
+			@Override
+			public void handle(WorkerStateEvent t){
+				HashMap<List<Entry>,List<Exit>> hs = task.getValue();
+				Platform.runLater(new Runnable() {
+		            public void run() {
+		            	for (Map.Entry<List<Entry>, List<Exit>> entry : hs.entrySet())
+		            	{
+		            	    System.out.println(entry.getKey() + "/" + entry.getValue());
+		            	}
+		            }
+		        });
+			}
+		});	 
 	}
 	
 	@FXML
