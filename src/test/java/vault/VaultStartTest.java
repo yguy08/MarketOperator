b82595a487732fe07e6bdb6279ec0858177373ff
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-12 16:25:39 -0400

    Add files via upload

--- src/test/java/vault/VaultStartTest.java
+++ src/test/java/vault/VaultStartTest.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import market.DigitalMarket;
+import market.Market;
+import market.MarketsEnum;
 import vault.main.VaultMainControl;
 
 public class VaultStartTest extends Application {
@@ -16,11 +19,16 @@ public class VaultStartTest extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		Market market = new DigitalMarket();
+		market.setMarketName(MarketsEnum.BITCOIN_OFFLINE.getMarketName());
+		market.setOfflineAssetList();
 		VaultMainControl customControl = new VaultMainControl();
         stage.setScene(new Scene(customControl));
         stage.setTitle("Speculation 1000");
         stage.getIcons().add(icon);
         stage.show();
+        customControl.setMarket(market);
+        customControl.setInitialTableView();
 	}
 
 }
