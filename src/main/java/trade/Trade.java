9dca6e2004278645250224632c477b3364aefd59
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-13 17:08:29 -0400

    Add files via upload

--- src/main/java/trade/Trade.java
+++ src/main/java/trade/Trade.java
@@ -1,17 +1,60 @@
 package trade;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
 import entry.Entry;
-import market.Market;
+import exit.Exit;
 import speculator.Speculator;
+import util.DateUtils;
 
-public interface Trade {
+public class Trade {
 	
-	//set new entries..take in a market with a speculators profile (entry days, risk, etc..)
-	public void setNewEntries(Market market, Speculator speculator);
+	Entry entry;
 	
-	//get new entries
-	public List<Entry> getNewEntries();
+	Exit exit;
+	
+	Speculator speculator;
 
+	public Trade(Speculator speculator){
+		this.entry = entry;
+		this.exit = exit;
+		this.speculator = speculator;
+	}
+	
+	
+	
+	public static List<String> runBackTest(List<Exit> exitList, Speculator speculator){
+		Date startDate = exitList.get(0).getEntryDate();
+		int days = DateUtils.getNumDaysFromDateToToday(startDate);
+		List<Entry> unitList = new ArrayList<>();
+		List<String> resultsList = new ArrayList<>();
+		int maxUnits = speculator.getMaxUnits();
+		for(int z = 0; z <= days; z++){
+			Date currentDateOfBacktest = DateUtils.addDays(startDate, z);
+			for(Exit exit : exitList){
+				Date closeDate = exit.getDateTime();
+				if(closeDate.equals(currentDateOfBacktest)){
+					for(int k = 0; k < unitList.size();k++){
+						if(exit.getEntry().equals(unitList.get(k)) && exit.isExit()){
+							unitList.remove(unitList.get(k));
+							resultsList.add("Exit: " + exit.toString());
+							System.out.println("New Exit " + exit.toString());
+						}else if (exit.getEntry().equals(unitList.get(k)) && exit.isOpen()){
+							resultsList.add("Open: " + exit.toString());
+							System.out.println("Still Open " + exit.toString());
+						}
+					}					
+				}
+				
+				if(exit.getEntryDate().equals(currentDateOfBacktest) && unitList.size() <= maxUnits){
+					unitList.add(exit.getEntry());
+					System.out.println("New Entry " + exit.getEntry().toString());
+					resultsList.add("New Entry " + exit.getEntry().toString());
+				}
+			}
+		}		
+		return resultsList;
+	}
+	
 }
