9f964654ad05e7fe527193008dc6fe975bb8e729
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-14 17:04:49 -0400

    Add files via upload

--- src/main/java/trade/Trade.java
+++ src/main/java/trade/Trade.java
@@ -1,60 +1,95 @@
 package trade;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import entry.Entry;
-import exit.Exit;
+import java.util.Random;
+
+import price.Entry;
+import price.Exit;
+import price.PriceData;
 import speculator.Speculator;
 import util.DateUtils;
+import util.Tuple;
 
 public class Trade {
 	
-	Entry entry;
-	
-	Exit exit;
-	
 	Speculator speculator;
+	
+	List<Tuple<List<Entry>, List<Exit>>> entryExit = new ArrayList<>();
 
 	public Trade(Speculator speculator){
-		this.entry = entry;
-		this.exit = exit;
 		this.speculator = speculator;
 	}
-	
-	
-	
-	public static List<String> runBackTest(List<Exit> exitList, Speculator speculator){
+
+	public void setEntryExitList(List<Exit> exitList) {
 		Date startDate = exitList.get(0).getEntryDate();
 		int days = DateUtils.getNumDaysFromDateToToday(startDate);
-		List<Entry> unitList = new ArrayList<>();
-		List<String> resultsList = new ArrayList<>();
-		int maxUnits = speculator.getMaxUnits();
 		for(int z = 0; z <= days; z++){
+			List<Entry> entries = new ArrayList<>();
+			List<Exit> exits = new ArrayList<>();
 			Date currentDateOfBacktest = DateUtils.addDays(startDate, z);
 			for(Exit exit : exitList){
 				Date closeDate = exit.getDateTime();
 				if(closeDate.equals(currentDateOfBacktest)){
-					for(int k = 0; k < unitList.size();k++){
-						if(exit.getEntry().equals(unitList.get(k)) && exit.isExit()){
-							unitList.remove(unitList.get(k));
-							resultsList.add("Exit: " + exit.toString());
-							System.out.println("New Exit " + exit.toString());
-						}else if (exit.getEntry().equals(unitList.get(k)) && exit.isOpen()){
-							resultsList.add("Open: " + exit.toString());
-							System.out.println("Still Open " + exit.toString());
-						}
-					}					
+					exits.add(exit);
 				}
 				
-				if(exit.getEntryDate().equals(currentDateOfBacktest) && unitList.size() <= maxUnits){
-					unitList.add(exit.getEntry());
-					System.out.println("New Entry " + exit.getEntry().toString());
-					resultsList.add("New Entry " + exit.getEntry().toString());
+				Date entryDate = exit.getEntryDate();
+				if(entryDate.equals(currentDateOfBacktest)){
+					entries.add(exit.getEntry());
 				}
 			}
-		}		
+			entryExit.add(new Tuple<List<Entry>, List<Exit>>(entries,exits));
+		}
+	}
+	
+	public List<String> runBackTest(){
+		List<Entry> unitList = new ArrayList<>();
+		List<String> resultsList = new ArrayList<>();
+		Random RANDOM = new Random();
+		for(Tuple<List<Entry>,List<Exit>> entryExit : entryExit){
+			for(Exit exit : entryExit.b){
+				if(unitList.contains(exit.getEntry()) && !(exit.isOpen())){
+					unitList.remove(exit.getEntry());
+					speculator.setAccountBalance(exit.calcGainLossAmount());
+					resultsList.add(exit.toString() + " " + speculator.getAccountBalance());
+					System.out.println("New Exit! " + exit.toString());
+				}
+			}
+			
+			Collections.sort(entryExit.a, new Comparator<Entry>() {
+			    @Override
+				public int compare(Entry o1, Entry o2) {
+			        return o2.getVolume().compareTo(o1.getVolume());
+			    }
+			});
+			
+			//for entry e : entry exit a...
+			for(Entry entry : entryExit.a){
+				if(unitList.size() > speculator.getMaxUnits()){
+					break;
+				}else if(speculator.isSortVol()){
+					unitList.add(entry);
+					System.out.println(entry.toString());
+					resultsList.add(entry.toString());
+				}else{
+					int r = RANDOM.nextInt(entryExit.a.size());
+					unitList.add(entryExit.a.get(r));
+					System.out.println(entryExit.a.get(r).toString());
+					resultsList.add(entryExit.a.get(r).toString());
+				}
+			}
+		}
+		
 		return resultsList;
+	}
+
+	public List<Tuple<List<Entry>, List<Exit>>> getEntryExit() {
+		return entryExit;
 	}
 	
 }
