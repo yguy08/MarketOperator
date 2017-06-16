889f0f46ebe4f9eb9b8e9cd1b93a761584bfcf51
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-16 13:11:14 -0400

    Add files via upload

---
+++ src/test/java/speculator/SpeculatorJunit.java
@@ -0,0 +1,14 @@
+package speculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SpeculatorJunit {	
+	
+	@Test
+	public void testCreateBitcoinMarket(){
+		
+	}
+
+}
