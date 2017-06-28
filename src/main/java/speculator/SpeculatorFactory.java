a6ea71b7f0d776820c44c0029d75607608b797fd
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-28 17:08:01 -0400

    Update SpeculatorFactory.java

--- src/main/java/speculator/SpeculatorFactory.java
+++ src/main/java/speculator/SpeculatorFactory.java
@@ -1,17 +1,19 @@
 package speculator;
 
 import market.MarketsEnum;
+import vault.Config;
 
 public class SpeculatorFactory {
 	
 	//set speculator
-	public static Speculator createSpeculator(String marketName, int balance, int risk, int maxUnits, 
+	public static Speculator createSpeculator(int balance, int risk, int maxUnits, 
 			int stopLength, int timeFrameDays, int entryFlag, int exitFlag, boolean longOnly, boolean sortVol){
 		
-		if(marketName.equalsIgnoreCase(MarketsEnum.BITCOIN.getMarketName())){
+		if(Config.getMarket().getMarketName().equalsIgnoreCase(MarketsEnum.BITCOIN.getMarketName())){
 			return new DigitalSpeculator(balance, risk, maxUnits, stopLength, timeFrameDays, entryFlag, exitFlag, longOnly, sortVol);
-		}
-		
+		}else if(Config.getMarket().getMarketName().equalsIgnoreCase(MarketsEnum.BITCOIN_OFFLINE.getMarketName())){
+			return new DigitalSpeculator(balance, risk, maxUnits, stopLength, timeFrameDays, entryFlag, exitFlag, longOnly, sortVol);
+		}		
 		return null;
 	}
 	
