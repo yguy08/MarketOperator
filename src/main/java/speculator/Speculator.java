e18b25537d8344c7eef8266c1681aa22520138c2
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-30 17:22:09 -0400

    Update Speculator.java

--- src/main/java/speculator/Speculator.java
+++ src/main/java/speculator/Speculator.java
@@ -1,9 +1,6 @@
 package speculator;
 
 import java.math.BigDecimal;
-import java.util.List;
-
-import trade.Entry;
 
 public interface Speculator {
 	
@@ -15,61 +12,12 @@ public interface Speculator {
 	
 	public BigDecimal getStartAccountBalance();
 	
-	public void setRisk(BigDecimal percent);
-	
-	public BigDecimal getRisk();
-	
-	public void setEntrySignalDays(int numDays);
-	
-	public int getEntrySignalDays();
-	
-	public void setSellSignalDays(int numDays);
-	
-	public int getSellSignalDays();
-	
-	public void setMaxUnits(int numUnits);
-	
-	public int getMaxUnits();
-	
-	public void setStopLength(BigDecimal multiplier);
-	
-	public BigDecimal getStopLength();
-	
-	public void setMinVolume(BigDecimal volumeLimit);
-	
-	public BigDecimal getMinVolume();
-	
 	public void setTotalReturnAmount(BigDecimal amount);
 	
 	public BigDecimal getTotalReturnAmount();
 	
 	public void setTotalReturnPercent();
 	
-	public BigDecimal getTotalReturnPercent();
-	
-	public void setTimeFrameDays(int numDays);
-	
-	public int getTimeFrameDays();
-	
-	public List<Entry> getEntryList();
-	
-	public void setEntryList(List<Entry> entryList);
-	
-	public boolean isLongOnly();
-	
-	public void setLongOnly(boolean isLongOnly);
-	
-	public boolean isSortVol();
-	
-	public void setSortVol(boolean sortVol);
-	
-	public static int getPriceHistoryYears(){
-		return 10;
-	}
-	
-	public static int getMovingAvg(){
-		return 20;
-	}
-	
+	public BigDecimal getTotalReturnPercent();	
 	
 }
