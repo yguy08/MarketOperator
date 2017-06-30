33238fe982efb7114c5dce8188980ccb6fd6f555
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-30 17:20:34 -0400

    Update BitcoinMarket.java

--- src/main/java/market/BitcoinMarket.java
+++ src/main/java/market/BitcoinMarket.java
@@ -61,7 +61,7 @@ public class BitcoinMarket implements Market {
 		List<CurrencyPair> currencyPairs = exchange.getExchangeSymbols();
 		for(CurrencyPair currencyPair : currencyPairs){
 			if(currencyPair.toString().endsWith("BTC")){
-				asset = AssetFactory.createAsset(this, currencyPair.toString());
+				asset = AssetFactory.createAsset(currencyPair.toString());
 				assetList.add(asset);
 			}
 		}
@@ -76,7 +76,7 @@ public class BitcoinMarket implements Market {
 		try {
 			currencyPairs = Files.readAllLines(Paths.get(resourceUrl.toURI()));
 			for(String currencyPair : currencyPairs){
-				asset = AssetFactory.createAsset(this, currencyPair);
+				asset = AssetFactory.createAsset(currencyPair);
 				assetList.add(asset);
 			}
 		} catch (IOException | URISyntaxException e) {
