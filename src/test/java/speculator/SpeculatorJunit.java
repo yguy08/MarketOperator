95c95b0d7412398573a87488078044d293760795
Wyatt Endres <wyatt.endres@gmail.com>
2017-06-30 17:27:37 -0400

    Update SpeculatorJunit.java

--- src/test/java/speculator/SpeculatorJunit.java
+++ src/test/java/speculator/SpeculatorJunit.java
@@ -2,12 +2,22 @@ package speculator;
 
 import org.junit.Test;
 
+import vault.Config;
+
 import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
 
 public class SpeculatorJunit {	
 	
 	@Test
-	public void testCreateBitcoinMarket(){
+	public void testAccountBal(){
+		Config.TestConfig();
+		Speculator speculator = new DigitalSpeculator();
+		assertEquals(Config.getAccountBalance(),speculator.getAccountBalance());
+		speculator.setAccountBalance(new BigDecimal(1.00));
+		assertEquals(1,speculator.getAccountBalance().compareTo(Config.getAccountBalance()));
+		assertEquals(Config.getAccountBalance(),speculator.getStartAccountBalance());
 		
 	}
 
