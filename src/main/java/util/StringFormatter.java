7b9f61dab5a23b7044eb6afa1c4c00bf6f4591f8
Wyatt Endres <wyatt.endres@gmail.com>
2017-07-11 17:16:25 -0400

    pretty sats, pretty usd

--- src/main/java/util/StringFormatter.java
+++ src/main/java/util/StringFormatter.java
@@ -38,5 +38,18 @@ public class StringFormatter {
 			return bigFormat;
 		}		
 	}
+	
+	public static String prettySatsPrice(BigDecimal price){
+		price = price.movePointRight(8);
+		String pattern = "###,###,###";
+		String number = new DecimalFormat(pattern).format(price);
+		return number;
+	}
+	
+	public static String prettyUSDPrice(BigDecimal price){
+		String pattern = "0.00";
+		String number = new DecimalFormat(pattern).format(price);
+		return number;
+	}
 
 }
