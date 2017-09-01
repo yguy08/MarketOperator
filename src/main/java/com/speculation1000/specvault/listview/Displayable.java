be80f71236b39fe1d4407fa7b9febdc11a066394
yguy08 <wyatt.endres@gmail.com>
2017-09-01 16:47:21 -0400

    gradle gitignore

--- src/main/java/com/speculation1000/specvault/listview/Displayable.java
+++
@@ -1,5 +0,0 @@
-package com.speculation1000.specvault.listview;
-
-public interface Displayable {
-	
-}
--- src/main/resources/sql/latest.sql
+++
@@ -1,7 +0,0 @@
-SELECT Base,Counter,Exchange,Date,Max(Close), Volume
-FROM markets 
-WHERE Base = Base 
-AND Counter = Counter 
-AND Date > 1503000090 
-GROUP BY Base,Counter,Exchange,Date 
-ORDER BY Counter ASC
